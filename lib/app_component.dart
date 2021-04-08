d82b1c61e4ddfd5210e8b3c0ab91d3c602e189eb
GMChristo <gabrielchristo@hotmail.com>
2021-04-08 10:02:08 -0300

    2.Master/Detail - add mostrar info do char selecionado

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,23 +1,20 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_tour_of_heroes/src/hero.dart';
+import 'package:angular_tour_of_heroes/src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
-  // templateUrl: 'app_component.html',
-  template: '''
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}}</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  ''',
-  directives: [formDirectives],
+  templateUrl: 'app_component.html',
+  directives: [
+    formDirectives,
+    coreDirectives,
+  ],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  var hero = Hero(1, 'Windstorme');
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+  void onSelect(Hero hero) => selected = hero;
 }
