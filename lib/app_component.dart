763f00fba07d233a0c22f66dd87328e54ab82377
GMChristo <gabrielchristo@hotmail.com>
2021-04-08 11:22:34 -0300

    4.Services - criado service de Hero, OnInit e Future para uso de aync

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,22 +1,29 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_tour_of_heroes/src/hero.dart';
-import 'package:angular_tour_of_heroes/src/mock_heroes.dart';
 import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [
-    formDirectives,
-    coreDirectives,
-    HeroComponent
-  ],
+  directives: [formDirectives, coreDirectives, HeroComponent],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+class AppComponent implements OnInit {
   final title = 'Tour of Heroes';
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
   Hero selected;
   void onSelect(Hero hero) => selected = hero;
+
+  final HeroService _heroService;
+  AppComponent(this._heroService);
+
+  @override
+  void ngOnInit() => _getHeroes();
+
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAll();
+  }
 }
