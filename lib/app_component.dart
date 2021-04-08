1ecc046111942127a9ee98b60865147d844d5613
GMChristo <gabrielchristo@hotmail.com>
2021-04-08 14:05:33 -0300

    5.Routing - add a dashboard - configurado a rota para ter uma pagina inicial e add o dashboard

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -5,9 +5,16 @@ import 'src/routes.dart';
 
 @Component(
   selector: 'my-app',
-  template: ''' ···
-    <router-outlet [routes]="Routes.all"></router-outlet>
-  ''',
+  template: '''
+  <h1>{{title}}</h1>
+  <nav>
+    <a [routerLink]="RoutePaths.dashboard.toUrl()"
+       [routerLinkActive]="'active'">Dashboard</a>
+    <a [routerLink]="RoutePaths.heroes.toUrl()"
+       [routerLinkActive]="'active'">Heroes</a>
+  </nav>
+  <router-outlet [routes]="Routes.all"></router-outlet>
+''',
   directives: [routerDirectives],
   providers: [ClassProvider(HeroService)],
   exports: [RoutePaths, Routes],
