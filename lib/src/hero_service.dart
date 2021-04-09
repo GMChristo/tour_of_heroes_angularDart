0eb2dbaffe19c2bf476a6abaa2de5e4c6589027d
GMChristo <gabrielchristo@hotmail.com>
2021-04-09 15:26:01 -0300

    6.HTTP - Providing/Register HTTP Services - Heroes and HTTP part1

--- lib/src/hero_service.dart
+++ lib/src/hero_service.dart
@@ -1,8 +1,38 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
 import 'hero.dart';
-import 'mock_heroes.dart';
 
 class HeroService {
-  Future<List<Hero>> getAll() async => mockHeroes;
+
+  static const _heroesUrl = 'api/heroes'; // URL to web API
+
+final Client _http;
+
+HeroService(this._http);
+
+Future<List<Hero>> getAll() async {
+  try {
+    final response = await _http.get(_heroesUrl);
+    final heroes = (_extractData(response) as List)
+        .map((json) => Hero.fromJson(json))
+        .toList();
+    return heroes;
+  } catch (e) {
+    throw _handleError(e);
+  }
+}
+
+dynamic _extractData(Response resp) => json.decode(resp.body)['data'];
+
+Exception _handleError(dynamic e) {
+  print(e); // for demo purposes only
+  return Exception('Server error; cause: $e');
+}
+
   Future<Hero> get(int id) async =>
     (await getAll()).firstWhere((hero) => hero.id == id);
+    
 }
